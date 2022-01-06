95f963b20985229ca7c22b3934ac2fb2a3815daf
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-06 13:38:50 -0500

    Added basic function for operations.
    Added the 4 basic math functions and an operation executor.

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+// Basic Math Functions
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// Execute operation from calculator
+function operate(num1, num2, operation) {
+    switch (operation) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return 'Syntax Error!';
+    }
+}
