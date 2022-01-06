0f00ba040e07374b4e8cabced1b552774b6ee810
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-06 18:05:04 -0500

    Added functionality to the buttons
    Added functions to either display on screen or calculate a result.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,61 @@
+// Initializing variables
+let screenContent = '';
+let isResult = false;
+
+const screen = document.querySelector('.screen');
+
+// Populate keys with functions
+function keyPressed(e) {
+    let keyText = e.target.textContent;
+
+    let current = screenContent.trim().split(' ').map(value => !isNaN(value) ? Number(value) : value);
+
+    if (isNaN(keyText)) {
+        if (screenContent === '') {
+            return;
+        } else if (keyText === 'C') {
+            screenContent = '';
+            isResult = false;
+        } else if (keyText === '=') {
+            if (current.length === 1 || current.length % 2 === 0) {
+                return;
+            } else {
+                while (current.length > 1) {
+                    let operation = current.splice(0, 3);
+
+                    let result = operate(...operation);
+
+                    current.unshift(result);
+                }
+
+                screenContent = current[0].toString();
+                isResult = true;
+            }
+        } else {
+            if (isNaN(current.at(-1))) {
+                screenContent = `${current.slice(0, -1).join(' ')} ${keyText} `;
+            } else {
+                screenContent = `${screenContent} ${keyText} `;
+            }
+
+            isResult = false;
+        }
+    } else {
+        if (isResult) {
+            screenContent = keyText;
+            isResult = false;
+        } else {
+            screenContent += keyText;
+        }
+    }
+
+    screen.textContent = screenContent;
+}
+
+const keys = document.querySelectorAll('.key, .key-2, .key-3');
+
+keys.forEach(key => key.addEventListener('click', keyPressed));
+
 // Basic Math Functions
 
 function add(a, b) {
@@ -13,19 +71,19 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    return b === 0 ? 'Mathematical Error!' : Math.round((a / b) * 100) / 100;
 }
 
 // Execute operation from calculator
-function operate(num1, num2, operation) {
+function operate(num1, operation, num2) {
     switch (operation) {
         case '+':
             return add(num1, num2);
         case '-':
             return subtract(num1, num2);
-        case '*':
+        case '×':
             return multiply(num1, num2);
-        case '/':
+        case '÷':
             return divide(num1, num2);
         default:
             return 'Syntax Error!';
